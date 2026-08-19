161d63bf9425b40409fcca38007c914f29456175
mtravaillard <maxim.travaillard@gmail.com>
2026-08-19 15:38:11 +0200

    Add a new example with a SystemRDL map

---
+++ test/basic_regs/basic_regs.sv
@@ -0,0 +1,16 @@
+module basic_regs (
+    input  logic       clk,
+    input  logic       rst_n,
+
+    input  logic       enable,
+    output logic [7:0] count
+);
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            count <= '0;
+        else if (enable)
+            count <= count + 1'b1;
+    end
+
+endmodule
